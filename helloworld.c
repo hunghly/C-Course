2c29f90e16364a828a7a4f886dc7e519e1850c38
hunghly <hunghly@gmail.com>
2020-09-21 21:46:12 -0500

    first commit

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf() displays the string inside the quotation
+    printf("Hello, World!\n");
+    printf("Hello, Hung!\n");
+    printf("Hello, C!\n");
+    return 0;
+}
