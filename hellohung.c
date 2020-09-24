c6c50792c45c437f4ffaf2a85213ff0d690fc160
hunghly <hunghly@gmail.com>
2020-09-23 23:12:56 -0500

    messed with scanf feature

--- hellohung.c
+++ hellohung.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include "test.h"
 
 int main() {
 
-    printf("Hello Hung!");
+    // printf("Hello Hung!\n");
+    // printf("%d + %d = %d", 1, 2, add(1,2));
+
+    /* scanf will:
+        1. return the number of items that it successfully reads
+        2. if scanf reads a value for one of the basic variable types, precede the variable name with an &
+        3. if you use scanf to read a string into a character array, don't use an & */
+    char str[100]; // declare a string/char array of 100
+    int i; // declare an int i
+
+    // printf("Enter a value: ");
+    // scanf("%s %d", str, &i);
+
+    // printf("\nEnter entered: %s %d ", str, i);
+    // int num = scanf(stdin);
+    // printf("%d", num);
+
+    int num1;
+    int num2;
+
+    printf("Please enter a value for num1: ");
+    scanf("%d", &num1);
+    printf("\nPlease enter a value for num2: ");
+    scanf("%d", &num2);
+
+    printf("%d + %d = %d\n", num1, num2, add(num1, num2));
     return 0;
 
 }
