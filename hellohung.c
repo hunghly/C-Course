99cb8a12f546ee3660e05a6a52a1b90d8c9060ba
hunghly <hunghly@gmail.com>
2020-09-27 22:33:51 -0500

    commit

--- hellohung.c
+++ hellohung.c
@@ -23,12 +23,12 @@ int main() {
     int num1;
     int num2;
 
-    printf("Please enter a value for num1: ");
-    scanf("%d", &num1);
-    printf("\nPlease enter a value for num2: ");
-    scanf("%d", &num2);
+    // printf("Please enter a value for num1: ");
+    // scanf("%d", &num1);
+    // printf("\nPlease enter a value for num2: ");
+    // scanf("%d", &num2);
 
-    printf("%d + %d = %d\n", num1, num2, add(num1, num2));
-    return 0;
+    // printf("%d + %d = %d\n", num1, num2, add(num1, num2));
+    // return 0;
 
 }
