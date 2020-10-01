1ed394a82d64f167e745067fe68839de596deef3
hunghly <hunghly@gmail.com>
2020-10-01 11:05:44 -0500

    added matrix

---
+++ Arrays/multi-dim-array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int matrix[4][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9},
+        //{0,0,0} // this last row will automatically initialize to 0 if we leave it off.
+        [3][0] = 20,
+    };
+
+    printf("%d\n", matrix[1][1]);
+    printf("%d\n", matrix[3][1]);
+    printf("%d\n", matrix[3][0]);
+
+    return 0;
+}
