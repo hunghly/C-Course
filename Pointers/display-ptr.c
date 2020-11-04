d6fd955439bb286a7057350927448080584fc53f
hunghly <hunghly@gmail.com>
2020-11-04 17:43:22 -0600

    worked on more pointer testing

--- Pointers/display-ptr.c
+++ Pointers/display-ptr.c
@@ -14,4 +14,5 @@ int main() {
     // the cast to void* is used to prevent a possible warning from the compiler
     // the %p specification is expecting a value to be some kind of pointer type, but the type of the &pnumber is "pointer to a pointer to int"
     printf("pnumber's address is: %p\n", (void*)&pnumber); //output the address of the pointer
+    printf("pnumber's size is: %d bytes\n", (int) sizeof(pnumber));
 }
