a7921f2cf28ab58fa6cabbb71507bca49a68397a
hunghly <hunghly@gmail.com>
2020-11-05 15:29:10 -0600

    completed challenge

---
+++ Pointers/pointer-challenge.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // write a program that creates an int variable with hard coded value, assign that variable's address to a pointer variable
+    // display as output the address of the pointer, the value of the porinter, and the value of what the pointer is pointing to
+
+    int number = 25; // create an int w/ hardcoded value
+    int *pnumber = &number; // assign pointer variable to address of number
+
+    printf("the address of pointer is %p\n", (void*)&pnumber);
+    printf("the value of pointer is %p\n", pnumber);
+    printf("the value of what the pointer is pointing to is %d\n", *pnumber);
+
+    // pointer has 3 values
+    // the pointer's address
+    // the pointer's value (which is the address of what the pointer is pointing to)
+    // the pointer's target's value
+}
