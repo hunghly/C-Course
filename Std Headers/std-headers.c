7856c4e733bb16b85ec5e4d4e24dd68493aacf06
hunghly <hunghly@gmail.com>
2020-11-11 18:21:17 -0600

    added headers

---
+++ Std Headers/std-headers.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <limits.h>
+#include <stdbool.h>
+
+// <stddef.h>
+// defines NULL, offsetof, ptrdiff_t, size_t, wchar_t
+
+// <limits.h>
+// defines many constants
+
+// <stdbool.h>
+// defines defs for booleans
